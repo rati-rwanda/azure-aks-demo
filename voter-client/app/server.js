d6e14cfa64caa796178563fc7e37fe09bbba4824
rati-rwanda <159041709+rati-rwanda@users.noreply.github.com>
2024-02-13 09:57:49 +0200

    Update server.js

--- voter-client/app/server.js
+++ voter-client/app/server.js
@@ -4,7 +4,7 @@
 
 var express = require('express');
 var serveStatic = require('serve-static');
-var port = process.env.PORT || 8080;
+var port = 3000;
 var app = express();
 
 app.use(serveStatic(__dirname, {'index': ['index.html']}));
